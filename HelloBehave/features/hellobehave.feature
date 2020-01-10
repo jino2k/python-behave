05c70a3b15bdd53035ab0c9168e08ec78d7a7586
jino2.lee <jino2.lee@lge.com>
2020-01-10 16:56:10 +0900

    added HelloBehave

---
+++ HelloBehave/features/hellobehave.feature
@@ -0,0 +1,10 @@
+Feature: showing off behave
+
+  Scenario: run a simple test
+     Given we have behave installed
+
+      When we implement a test
+      Then behave will test it for us!
+
+      When we implement another test
+      Then let me know it!
---
+++ HelloBehave/features/steps/HelloBehave.py
@@ -0,0 +1,21 @@
+from behave import *
+
+@given('we have behave installed')
+def step_impl(context):
+    pass
+
+@when('we implement a test')
+def step_impl(context):
+    assert True is not False
+
+@then('behave will test it for us!')
+def step_impl(context):
+    assert context.failed is False
+
+@when('we implement another test')
+def step_impl(context):
+    assert True is not False
+
+@then('let me know it!')
+def step_impl(context):
+    assert context.failed is False
